6ca5abd1b0544bf5cb5af8e09c802f0587927e0a
Nathan Broadbent <nathan.f77@gmail.com>
2012-05-12 18:28:35 +1200

    Fixed app_observer and wrote tests

--- lib/fat_free_crm/cloudfuji/event_observers/app_observer.rb
+++ lib/fat_free_crm/cloudfuji/event_observers/app_observer.rb
@@ -3,25 +3,34 @@ module FatFreeCRM
     module EventObservers
       class AppObserver < ::Cloudfuji::EventObserver
         def app_claimed
-          if user = User.find_by_email(data['email'])
-            puts "Updating #{User.first.inspect} with incoming data #{params.inspect}"
-          else
-            user = User.new
-            puts "Creating User with incoming data #{params.inspect}"
-          end
-
-          puts "Authlogic username column: #{::Authlogic::Cas.cas_username_column}="
-          puts "Setting username to: #{params.try(:[], 'ido_id')}"
+          # Be verbose in development environment
+          debug = Rails.env == 'development'
 
           data = params['data']
+          ido_id = data.try(:[], 'ido_id')
+
+          if user = User.find_by_email(data['email'])
+            puts "Updating #{user.inspect} with incoming data #{params.inspect}" if debug
+          else
+            user = User.new
+            puts "Creating User with incoming data #{params.inspect}" if debug
+          end
+
+          puts "Authlogic username column: #{::Authlogic::Cas.cas_username_column}=" if debug
+          puts "Setting username to: #{ido_id}" if debug
+
           user.email      = data['email']
-          user.first_name = user.email.split('@').first
-          user.last_name  = user.email.split('@').last
-          user.username   = data['email']
+          # Set first and last name from email if both blank
+          if user.first_name.blank? && user.last_name.blank?
+            user.first_name = user.email.split('@').first
+            user.last_name  = user.email.split('@').last
+          end
+          user.username   = ido_id
           user.deleted_at = nil
           user.admin = true
-          user.send("#{::Authlogic::Cas.cas_username_column}=".to_sym, data.try(:[], 'ido_id'))
-          puts user.inspect
+          user.send("#{::Authlogic::Cas.cas_username_column}=".to_sym, ido_id)
+
+          puts user.inspect if debug
           user.save!
         end
       end
--- spec/event_observers/app_observer_spec.rb
+++ spec/event_observers/app_observer_spec.rb
@@ -1,19 +1,39 @@
-require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 include FatFreeCRM::Cloudfuji::EventObservers
 
 describe AppObserver do
-  before do
-
-  end
-
   describe "App Claimed" do
+    before do
+      @observer = AppObserver.new
+      @observer.params = {
+        'data' => {
+          'ido_id' => '57a57c56b57d76e76f',
+          'email'  => 'cloudfuji_user@cloudfuji.com'
+        }
+      }
+    end
+
     it "should update a known user if app is claimed by a recognized email address" do
+      @user = FactoryGirl.create(:user, :email => 'cloudfuji_user@cloudfuji.com', :ido_id => "123456789")
+      original_name = [@user.first_name, @user.last_name]
+
+      @observer.app_claimed
+
+      # Should not create a new user
+      User.find_all_by_email('cloudfuji_user@cloudfuji.com').size.should == 1
+
+      @user.reload
+      [@user.first_name, @user.last_name].should == original_name
+      @user.ido_id.should == '57a57c56b57d76e76f'
     end
 
     it "should create a new user if app is claimed by an unrecognized email address" do
+      @observer.app_claimed
+
+      @user = User.find_by_email('cloudfuji_user@cloudfuji.com')
+      @user.first_name.should == "cloudfuji_user"
+      @user.last_name.should == "cloudfuji.com"
     end
-
   end
-
 end
