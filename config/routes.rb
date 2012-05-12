fb03dd2ec2ef6e131ed628a03a219e62a5dd42ab
Nathan Broadbent <nathan.f77@gmail.com>
2012-05-12 15:53:24 +1200

    Remove login & logout named routes before writing authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
+# Remove existing authentication routes
+r = Rails.application.routes
+r.set.routes.reject! {|r| %w(login logout).include? r.name }
+r.set.send "clear_cache!"
+
 begin
   Rails.application.routes.draw do
     cloudfuji_routes
+    cloudfuji_authentication_routes if Cloudfuji::Platform.on_cloudfuji?
   end
 rescue => e
   puts "Error loading the Cloudfuji routes:"
   puts "#{e.inspect}"
-end
-
-FatFreeCRM::Application.routes.draw do
-
-  cloudfuji_authentication_routes if Cloudfuji::Platform.on_cloudfuji?
-
 end
