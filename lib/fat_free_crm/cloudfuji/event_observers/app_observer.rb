c3febf8fdbc04b339bfb308a63f3566a9e5670cc
Nathan Broadbent <nathan.f77@gmail.com>
2012-05-12 17:05:55 +1200

    When app is claimed, don't modify the first User. Update user if found by email address, otherwise create new user.

--- lib/fat_free_crm/cloudfuji/event_observers/app_observer.rb
+++ lib/fat_free_crm/cloudfuji/event_observers/app_observer.rb
@@ -3,23 +3,26 @@ module FatFreeCRM
     module EventObservers
       class AppObserver < ::Cloudfuji::EventObserver
         def app_claimed
-          puts "Updating #{User.first.inspect} with incoming data #{params.inspect}"
+          if user = User.find_by_email(data['email'])
+            puts "Updating #{User.first.inspect} with incoming data #{params.inspect}"
+          else
+            user = User.new
+            puts "Creating User with incoming data #{params.inspect}"
+          end
+
           puts "Authlogic username column: #{::Authlogic::Cas.cas_username_column}="
           puts "Setting username to: #{params.try(:[], 'ido_id')}"
 
-          user = User.first
-          if user
-            data = params['data']
-
-            user.email      = data['email']
-            user.first_name = user.email.split('@').first
-            user.last_name  = user.email.split('@').last
-            user.username   = data['email']
-            user.deleted_at = nil
-            user.send("#{::Authlogic::Cas.cas_username_column}=".to_sym, params['data'].try(:[], 'ido_id'))
-            puts user.inspect
-            user.save!
-          end
+          data = params['data']
+          user.email      = data['email']
+          user.first_name = user.email.split('@').first
+          user.last_name  = user.email.split('@').last
+          user.username   = data['email']
+          user.deleted_at = nil
+          user.admin = true
+          user.send("#{::Authlogic::Cas.cas_username_column}=".to_sym, data.try(:[], 'ido_id'))
+          puts user.inspect
+          user.save!
         end
       end
     end
