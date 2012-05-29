41546b6cb2808db876ba670f853a9091121d1455
Nathan Broadbent <nathan.f77@gmail.com>
2012-05-29 21:25:00 +1200

    Added Ido ID field to lead edit form

--- lib/fat_free_crm/cloudfuji/view_hooks.rb
+++ lib/fat_free_crm/cloudfuji/view_hooks.rb
@@ -4,7 +4,11 @@ module FatFreeCRM
 
       # Add lead scoring to summary on lead show page
       def show_lead_sidebar_bottom(view, context)
-        view.render(:partial => 'leads/sidebar_show_lead_scoring', :locals => {:lead => context[:lead]})
+        view.render(:partial => 'leads/sidebar_show_lead_scoring', :locals => context)
+      end
+
+      def lead_top_section_bottom(view, context)
+        view.render(:partial => 'leads/top_section_ido_id', :locals => context)
       end
 
     end
